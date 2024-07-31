80207ff50c5510d352eb0e7489d2b361ce42a86d
iamchristiancollins <105734903+iamchristiancollins@users.noreply.github.com>
2024-07-31 11:19:32 -0500

    initialize app

---
+++ src/main/java/com/DumbChess/DumbChess/DumbChessApplication.java
@@ -0,0 +1,13 @@
+package com.DumbChess.DumbChess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumbChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumbChessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DumbChess/DumbChess/DumbChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DumbChess.DumbChess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumbChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
